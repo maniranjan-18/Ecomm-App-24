b153fc241b4143e20998c47bc483834cc6dd1859
maniranjan-18 <maniranjanpathak449@gmail.com>
2024-03-29 02:42:02 +0530

    scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Loading from "./components/loading";
+import Header from "./components/header";
 
 const Home = lazy(() => import("./pages/home"));
 const Cart = lazy(() => import("./pages/cart"));
@@ -27,6 +28,7 @@ const TransactionManagement = lazy(
 const App = () => {
   return (
     <Router>
+      <Header />
       <Suspense fallback={<Loading />}>
         <Routes>
           <Route path="/" element={<Home />} />
